8d8e6ff924d4693f24df2bb5ac50f55cdac3a68f
Carlosmyx <moranyaxcarlitos@gmail.com>
2025-05-28 12:10:22 -0600

    trabajo php

---
+++ ejemplophp/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menú</title>
+</head>
+<body>
+    <h1>Menu Principal</h1>
+    <h2>Regiones</h2>
+    <a href="basedatos/vista_regiones.php">Regiones</a>
+    <br>
+    <a href="basedatos/form_regiones_agregar.php">Agregar Región</a>
+</body>
+</html>
